907c2e6208d694df1ba69bb70a0333782e3dafe0
varuna2993 <saxenavaruna29@gmail.com>
2021-12-21 13:26:48 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,9 @@ export class HomeComponent {
 
   public demoVariable:any = "gaurav";
   public demoVariable1:any = "varuna";
+  public isShown:any= false;
+  toggle(){
+    this.isShown = !this.isShown;
+  }
+  public arr:any=[{key:"val",model:"500"},{key:"fly",model:"kylie"}];
 }
---
+++ src/app/loginForm/loginForm.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-loginForm',
+  templateUrl: './loginForm.component.html',
+  styleUrls: ['./loginForm.component.css']
+})
+export class LoginFormComponent {
+    
+ 
+  title = 'SecondProject';
+
+  
+}
