4d8b00f1be186e024012cc483666460a20a5acbf
Gaurav Srivastava <gaurav.srivastava@karexpert.com>
2021-12-23 01:42:11 +0530

    table added in product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  constructor(public http: HttpClient){
+  }
   title = 'SecondProject';
+  public status :any =   '';
+  public arrayVAR:any = [];
+  public arrayOBJ= {first: "GAURAV",sec: "IS DADDY OF", third: "VARUNA SRIVASTAVA" };
+
+  ngOnInit() {
+    this.prepareArray();
+  }
+
+  prepareArray(){
+    for (let index = 1; index < 11; index++) {
+      this.arrayVAR.push(5*index);      
+    }
+  }
+
+  getStatus(){
+    this.http.get("https://6149c97007549f001755a5ca.mockapi.io/agent").subscribe((result:any) =>{
+
+
+      this.status = result.status;
+      console.log("data====>",result);
+
+    },(err:any)=>{
+      console.log("EROOR====>",err);
+    });
+  }
+
+  
 }
