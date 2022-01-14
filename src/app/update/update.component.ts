0680db5097b660cf73fa0843bc4de2db34003048
Gaurav Srivastava <gaurav.srivastava@karexpert.com>
2022-01-15 02:31:57 +0530

    changes

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -11,10 +11,6 @@ import { ServiceService } from '../service/service.services';
 })
 export class UpdateComponent {
   title = 'Update';
-  public id:any="";
-  public email:any="";
-  public first_name:any=""
-  public last_name:any=""
   public apiData:any="";
   public currentUserId:any = "";
   constructor(public http: HttpClient,public domSanitizer: DomSanitizer,private activeRoute: ActivatedRoute,public data:ServiceService, public route: Router) { 
@@ -24,17 +20,13 @@ ngOnInit(){
   this.currentUserId = this.activeRoute.snapshot.params["id"];
   console.log("this.currentUserId==>",this.currentUserId);
     this.getUserDetais(this.currentUserId);
-    this.id= this.data.mobile.id;
-       this.email= this.data.mobile.email;
-       this.first_name= this.data.mobile.first_name;
-       this.last_name= this.data.mobile.last_name;
 }
 getUserDetais(id:any){
       var rat= this.http.get("https://reqres.in/api/users/"+id);
      rat.subscribe((result:any) =>{
   
          // this.status = result.status;\
-         this.apiData=result;
+         this.apiData=result.data;
         console.log("data====>",result);
   
       },(err:any)=>{
